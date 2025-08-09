cdca33e3cf562405509865ffb552fd78d31b8cf6
rizaladiyanto <rizaladiyanto@users.noreply.github.com>
2025-08-10 01:27:34 +0700

    ubah vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import tailwindcss from '@tailwindcss/vite'
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  base: process.env.APP_URL || '/', // Sesuaikan dengan domain yang kamu pakai
 });
-    
